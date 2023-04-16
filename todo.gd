2ae8e5679c2087cf2a8f6f7392f033c4c32a4ef8
TheGreenOne <60050442+TheGreen0ne@users.noreply.github.com>
2023-04-17 01:15:19 +0200

    update todos

--- todo.gd
+++ todo.gd
@@ -1,1 +1,12 @@
+
 # maybe a tab per disga
+# config file in usr://
+# contain some path to executable e.g.
+# file:///c:/... 
+# file:///home/ 
+# cat /home/user/somefile.json
+
+# read output of created process
+# mb 1 profile per line
+# mb 1 gal per line
+# mb whole execution in single json object (?)
