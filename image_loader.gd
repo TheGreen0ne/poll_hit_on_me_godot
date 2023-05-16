36f850ad8f5b5aa48653391b3f43132daf9d28e1
TheGreenOne <60050442+TheGreen0ne@users.noreply.github.com>
2023-05-16 16:59:10 +0200

    improve home path expansion

--- image_loader.gd
+++ image_loader.gd
@@ -7,7 +7,11 @@ var texture_file: String = "~/workspace/ui_fph/graphic/1280x720/fph_bon_enter.pn
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if texture_file.begins_with("~/"):
-		var home_path = "/".join(ProjectSettings.globalize_path("user://a").split("/").slice(0,3))
+		var home_path := OS.get_environment("HOME")
+		if home_path == "":
+			home_path = OS.get_environment("USERPROFILE")
+		if home_path == "":
+			home_path = "/".join(ProjectSettings.globalize_path("user://a").split("/").slice(0,3))
 		texture_file = home_path.path_join(texture_file.trim_prefix("~"))
 	if not FileAccess.file_exists(texture_file):
 		print("background not found")
@@ -15,7 +19,7 @@ func _ready() -> void:
 	var buffer := FileAccess.get_file_as_bytes(texture_file)
 	var err = FileAccess.get_open_error()
 	if err:
-		print(err)
+		print_debug(err)
 #	print("file size: ", buffer.size())
 	var img := Image.new()
 	img.load_png_from_buffer(buffer)
