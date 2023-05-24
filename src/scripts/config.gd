333b20c1947114ffa55b21f06e5fbc6e0b76321b
TheGreenOne <60050442+TheGreen0ne@users.noreply.github.com>
2023-05-24 11:42:47 +0200

    remove dead code

--- src/scripts/config.gd
+++ src/scripts/config.gd
@@ -11,11 +11,6 @@ func _enter_tree() -> void:
 ## Called when the node enters the scene tree for the first time.
 #func _ready() -> void:
 #	pass # Replace with function body.
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
 
 
 func load_config() -> void:
