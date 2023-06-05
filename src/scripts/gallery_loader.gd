ae44c3ab67293efc30bb9d63c52380289ec572f6
TheGreenOne <60050442+TheGreen0ne@users.noreply.github.com>
2023-06-05 09:31:37 +0200

    make all labels in gallery look the same

--- src/scripts/gallery_loader.gd
+++ src/scripts/gallery_loader.gd
@@ -1,6 +1,7 @@
 extends Node
 
-const GALLERY_ITEM_SCENE = preload("res://src/scenes/gallery_item.tscn")
+const GALLERY_ITEM_SCN = preload("res://src/scenes/gallery_item.tscn")
+const GALLERY_ITEM_LBL_SCN = preload("res://src/scenes/gallery_item_label.tscn")
 
 @export_node_path var gallery_item_parent_path := ^"../ScrollContainer/gallery"
 @export_node_path var loading_animation_path := ^"../loading_container"
@@ -54,18 +55,18 @@ func load_gallery() -> void:
 		else:
 #			print('"{0}" is no valid JSON object'.format([line]))
 			gallery_data = {"text": line}
-		_create_gallery(gallery_data)
+		_create_gallery_item(gallery_data)
 
 
-func _create_gallery(gallery_dict: Dictionary) -> void:
+func _create_gallery_item(gallery_dict: Dictionary) -> void:
 	if "text" in gallery_dict:
 		gallery_dict["text"] = StringUtils.apply_cr(gallery_dict["text"])
 	if "image" in gallery_dict:
-		var gal_item := GALLERY_ITEM_SCENE.instantiate()
+		var gal_item := GALLERY_ITEM_SCN.instantiate()
 		gal_item.image_path = gallery_dict["image"]
 		gal_item.label_text = gallery_dict["text"]
 		_gallery_item_parent.add_child(gal_item)
 	else:
-		var lbl := Label.new()
+		var lbl := GALLERY_ITEM_LBL_SCN.instantiate()
 		lbl.text = gallery_dict["text"]
 		_gallery_item_parent.add_child(lbl)
